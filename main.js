9d87efe5b0a6a69cfe6760b2f04cca8e00f0f520
SEscobar_9T <escobar.sa@obmontessori.edu.ph>
2025-04-18 23:30:04 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function menuchoice() {
+	var menuInput = document.getElementById('choiceFood').value;
+	
+	switch (parseInt(order)) {
+		case "B": 
+			document.getElementById('optionResult').innerHTML = "You ordered a Burger";
+			break;
+		
+		case "F": 
+			document.getElementById('optionResult').innerHTML = "You ordered Fries";
+			break;
+
+		case "S": 
+			document.getElementById('optionResult').innerHTML = "You ordered Soda";
+			break;
+
+		default:
+			document.getElementById('optionResult').innerHTML = "Invalid order";
+			break;
+		}
+
+
+}
